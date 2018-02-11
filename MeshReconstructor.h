628bf58fe696a0cc601409baa115c89081bf19dd
Romain PERRIN <romain.perrin@etu.unistra.fr>
2018-02-11 17:19:15 +0100

    starting normal computing

--- MeshReconstructor.h
+++ MeshReconstructor.h
@@ -8,14 +8,20 @@
  * 2018-2019
  */
 
+// glm
 #include "glm/glm.hpp"
 #include "glm/vec3.hpp"
 
 #include <vector>
 
+// Eigen
+#include "Eigen/Dense"
+
 #include "Vertex.h"
 #include "Mesh.h"
 #include "Octree.h"
+
+using namespace Eigen;
 
 /**
  * @brief The MeshReconstructor class
