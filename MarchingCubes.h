30b400c2b54955aee27d071293a1857c6796680f
Romain PERRIN <romain.perrin@etu.unistra.fr>
2018-03-03 13:48:54 +0100

    adding MarchingCubes class

--- MarchingCubes.h
+++ MarchingCubes.h
@@ -8,10 +8,29 @@
  * 2018-2019
  */
 
+#include "Vertex.h"
+
+// triangle structure used to represent a triangle (by representing 3 Vertices)
+typedef struct s_triangle
+{
+    Vertex p[3];
+} TRIANGLE;
+
+// structure used to represent a gridcell (8 vertices + 8 associated values)
+typedef struct s_gridcell
+{
+    Vertex p[8];
+    double val[8];
+} GRIDCELL;
+
+/**
+ * @brief The MarchingCubes class
+ */
 class MarchingCubes
 {
 protected:
     unsigned float _subdivisionFactor;
+    double _isolevel;
 
 public:
     // constructor(s)
@@ -23,6 +42,8 @@ public:
     void setSubdivisionFactor(unsigned float factor);
 
     // method(s)
+    int polygonise(GRIDCELL grid, double isolevel, TRIANGLE *triangles);
+    Vertex vertexInterpolate(double isolevel, Vertex p1, Vertex p2, double valp1, double valp2);
 };
 
 #endif // MARCHINGCUBES_H
